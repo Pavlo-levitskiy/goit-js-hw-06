809480d39daa6123c82dfb8448b138a9524bee3f
Pavlo-levitskiy <90378679+Pavlo-levitskiy@users.noreply.github.com>
2022-03-28 13:25:44 +0300

    goit-js-hw-06.

---
+++ src/js/task-02.js
@@ -0,0 +1,20 @@
+const ingredients = [
+  'Potatoes',
+  'Mushrooms',
+  'Garlic',
+  'Tomatos',
+  'Herbs',
+  'Condiments',
+];
+
+const ulList = document.querySelector("#ingredients");
+
+const liItem = ingredients.map((ingredient) => {
+  const listItem = document.createElement("li");
+  listItem.textContent = ingredient;
+  listItem.classList.add("item");
+  console.log(listItem);
+  return  listItem;
+});
+ulList.append(...liItem);
+  
